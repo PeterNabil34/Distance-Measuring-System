7a7ea38df3d8ada184547c69829fe6bca1301668
Peter Nabil <71277281+PeterNabil34@users.noreply.github.com>
2024-01-25 21:25:37 +0200

    Add files via upload

---
+++ Mini_Project_4/Mini_Project_4_WS/Distance_Measurement/Debug/Distance_Measurement.d
@@ -0,0 +1,8 @@
+Distance_Measurement.o Distance_Measurement.o: ../Distance_Measurement.c \
+  ../ultrasonic_sensor.h ../std_types.h ../lcd.h
+
+../ultrasonic_sensor.h:
+
+../std_types.h:
+
+../lcd.h:
